2df86ebcf8cb4749a93c75d5ec15ff19d71f0a56
Eleodorr <botnarencoeleodor8@gmail.com>
2023-05-09 11:28:26 +0300

    Add hoem page

---
+++ Booking.Persistance/Migrations/202304180904513_GenerateFirstEntities.cs
@@ -0,0 +1,96 @@
+﻿namespace Booking.Persistance.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class GenerateFirstEntities : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Barbershops",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                        Address = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.BaseUserEntities",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        LastName = c.String(),
+                        FirstName = c.String(),
+                        Username = c.String(),
+                        Password = c.String(),
+                        IsAdmin = c.Boolean(nullable: false),
+                        BarbershopId = c.Guid(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Barbershops", t => t.BarbershopId, cascadeDelete: true)
+                .Index(t => t.BarbershopId);
+            
+            CreateTable(
+                "dbo.Services",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        ServiceType = c.String(),
+                        Name = c.String(),
+                        Employee_Id = c.Guid(),
+                        Barbershop_Id = c.Guid(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.BaseUserEntities", t => t.Employee_Id)
+                .ForeignKey("dbo.Barbershops", t => t.Barbershop_Id)
+                .Index(t => t.Employee_Id)
+                .Index(t => t.Barbershop_Id);
+            
+            CreateTable(
+                "dbo.EmployeeAvailabilities",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        EmployeeId = c.Guid(nullable: false),
+                        Date = c.String(),
+                        Time = c.String(),
+                        SetByEmployee = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UserAppointments",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Phone = c.String(),
+                        BarbershopId = c.Guid(nullable: false),
+                        ServiceId = c.Guid(nullable: false),
+                        EmploeeAvId = c.Guid(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Services", "Barbershop_Id", "dbo.Barbershops");
+            DropForeignKey("dbo.BaseUserEntities", "BarbershopId", "dbo.Barbershops");
+            DropForeignKey("dbo.Services", "Employee_Id", "dbo.BaseUserEntities");
+            DropIndex("dbo.Services", new[] { "Barbershop_Id" });
+            DropIndex("dbo.Services", new[] { "Employee_Id" });
+            DropIndex("dbo.BaseUserEntities", new[] { "BarbershopId" });
+            DropTable("dbo.UserAppointments");
+            DropTable("dbo.EmployeeAvailabilities");
+            DropTable("dbo.Services");
+            DropTable("dbo.BaseUserEntities");
+            DropTable("dbo.Barbershops");
+        }
+    }
+}
---
+++ Booking.Persistance/Repository/ServiceRepository.cs
@@ -0,0 +1,65 @@
+﻿using Booking.BusinessLogic.Repository;
+using Booking.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Booking.Persistance.Context;
+using System.Data.Entity;
+
+
+namespace Booking.Persistance.Repository
+{
+    public class ServiceRepository: IServiceRepository
+    {
+        private readonly DataContext _context;
+
+        public ServiceRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Service>> GetAll()
+        {
+            var test = await _context.Services
+                .ToListAsync();
+
+            return test;
+        }
+
+        public async Task<Service> GetById(Guid id)
+        {
+            var service = await _context.Services
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            return service;
+        }
+
+        public void Add(Service item)
+        {
+            _context.Services.Add(item);
+        }
+
+
+        public void Update(Service item)
+        {
+            var barbershop = _context.Services.Find(item.Id);
+
+            if (barbershop != null)
+            {
+                barbershop.ServiceName = item.ServiceName;
+                barbershop.ServiceType = item.ServiceType;
+                _context.SaveChanges();
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            var barbershop = _context.Services.Find(id);
+
+            if (barbershop != null)
+                _context.Services.Remove(barbershop);
+        }
+    }
+}
